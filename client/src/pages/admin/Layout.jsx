f30368ac66cce5b7ef64e5bd99dcdca622a80600
faroutnerd <farhan91179@gmail.com>
2025-07-19 20:27:21 +0530

    static frontent completed

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -15,7 +15,7 @@ const Layout = () => {
     <>
       <div className="flex justify-between items-center py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200">
         <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' onClick={()=> navigate('/')} />
-        <button onClick={logout} className='text-sm px-8 bg-primary text-white rounded-full cursor-pointer'>Logout</button>
+        <button onClick={logout} className='text-sm px-8 bg-primary text-white rounded-full cursor-pointer py-2.5'>Logout</button>
       </div>
 
       <div className="flex h-[calc(100vh-70px)">
